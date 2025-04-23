dcec9cd01a115f13597ef2062bb9797656893191
Teddy <tewodrosanteneh@outlook.com>
2025-04-23 19:58:16 +0300

    version2

--- Main.java
+++ Main.java
@@ -2,17 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Print a message
-        System.out.println("Hello, World!");
 
-        // Get user input
         Scanner scanner = new Scanner(System.in);
         System.out.print("What's your name? ");
-        String name = scanner.nextLine();
+        String name = scanner.nextLine().trim();
 
         // Respond to user
         System.out.println("Nice to meet you, " + name + "!");
         
         scanner.close();
     }
+    
 }
